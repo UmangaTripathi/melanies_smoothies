b735cec26bca516533b03457967be715555d3f49
UmangaTripathi <56555230+UmangaTripathi@users.noreply.github.com>
2026-07-22 12:06:12 -0500

    Refactor smoothie app for clarity and functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,24 +8,23 @@ from snowflake.snowpark.functions import col
 st.title("🥤Customize Your Smoothie!🥤")
 
 # Add the subheader instruction text
-st.write(
-    """Choose the fruits you want in your custom Smoothie!"""
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
+# Connect to Snowflake on Streamlit Cloud
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+# User input for name on smoothie
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# Pull the fruit options dataframe including SEARCH_ON
+# Pull the fruit options dataframe including FRUIT_NAME and SEARCH_ON columns
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+# Convert the Snowpark Dataframe to a Pandas Dataframe to use the LOC function
 pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
 
+# Multiselect widget for selecting up to 5 fruits
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
@@ -38,13 +37,16 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
+        # Get the SEARCH_ON value corresponding to the chosen FRUIT_NAME
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
+        # Display nutrition information for each selected fruit using search_on value
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+    # Prepare SQL statement to insert order into Snowflake database
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
